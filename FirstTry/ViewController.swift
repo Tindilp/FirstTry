b82c1196be4d5ac1deef0953834ce151302d7909
Lucas <lucasdilorenzo_89@outlook.com>
2021-03-17 21:42:15 -0300

    initial commit

---
+++ FirstTry/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FirstTry
+//
+//  Created by Lucas Di Lorenzo on 01/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
